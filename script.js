81b05a6ab3e05e430ad3a7408fd39cdfd40166e1
JCharlyCas <charligallardo33@gmail.com>
2026-02-27 16:51:10 -0600

    Implement menu close on link click
    Added functionality to close the menu when a link is clicked.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menuToggle = document.getElementById("mobile-menu");
 const menu = document.getElementById("menu");
+const menuLinks = document.querySelectorAll(".menu a");
 
 menuToggle.addEventListener("click", function(){
     menu.classList.toggle("active");
+});
+
+// Cerrar menú al hacer clic en un enlace
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
 });
